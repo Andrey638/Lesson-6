a702d848f27dad37d3da77e47de56d4672fca8e3
Andrey3544 <box4435@mail.ru>
2022-12-16 14:37:04 +0300

    task43

---
+++ task41/Program.cs
@@ -0,0 +1,19 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+Console.WriteLine("количество больше 0 : {0}", quantity(array));
+
+int quantity(int[] array)
+{
+int count = 0;
+ for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+    return count;
+}
---
+++ task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x=0;
+double y=0;
+
+Console.WriteLine("две прямые пересекутся в точке с координатами X: {0}, Y: {1}",coordinataX(b1,k1,b2,k2),coordinataY(x,b2,k2));
+
+double coordinataX(double b1,double k1,double b2, double k2)
+{
+    {
+        x = (-b2 + b1)/(-k1 + k2);
+        return x;
+    }
+
+}    
+double coordinataY(double x,double b2, double k2)
+{    
+    {
+        y = k2 * x + b2;
+        return y;
+    }
+
+}
